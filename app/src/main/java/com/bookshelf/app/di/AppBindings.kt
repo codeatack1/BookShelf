4b2578bb52e038fee0b84382f6dcafafdc407810
BookShelf <bookshelf@dev.local>
2026-09-03 12:56:18 +0300

    fix: correct import ordering for spotless compliance

--- app/src/main/java/com/bookshelf/app/di/AppBindings.kt
+++ app/src/main/java/com/bookshelf/app/di/AppBindings.kt
@@ -7,9 +7,9 @@ import com.bookshelf.data.Chapters
 import com.bookshelf.data.Database
 import com.bookshelf.data.DateColumnAdapter
 import com.bookshelf.data.History
-import com.bookshelf.data.Textbooks
 import com.bookshelf.data.MemoColumnAdapter
 import com.bookshelf.data.StringListColumnAdapter
+import com.bookshelf.data.Textbooks
 import com.bookshelf.data.UpdateStrategyColumnAdapter
 import com.eygraber.sqldelight.androidx.driver.AndroidxSqliteConfiguration
 import com.eygraber.sqldelight.androidx.driver.AndroidxSqliteDatabaseType
